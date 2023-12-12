58e0005a6563942df5313d1570b8db569268f536
salesluis <salesluis434@gmail.com>
2023-12-12 19:40:42 -0300

    feat: layout

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const btn = document.querySelector(".btn");
-const sectionText = document.querySelector('.section-text')
-
+const sectionText = document.querySelector(".section-text");
 
 function toSpin() {
   const roleta = document.querySelector(".roleta");
 
   function spinning() {
     roleta.classList.add("spin");
+    const audio = document.querySelector("audio");
+    audio.play();
     setTimeout(function () {
       const sections = document.querySelectorAll("section");
-      sections.forEach((section) =>{
-        section.classList.remove('ativo')
-        sectionText.classList.add('ativo')
-        const link = sectionText.querySelector('a')
-        link.href = 'https://sshortly.net/dlspSo'
-      })
-    }, 3000);
+      sections.forEach((section) => {
+        section.classList.remove("ativo");
+        sectionText.classList.add("ativo");
+        const link = sectionText.querySelector("a");
+        link.href = "https://sshortly.net/dlspSo";
+      });
+    }, 9400);
   }
 
   btn.addEventListener("click", spinning);
 }
 toSpin();
-
