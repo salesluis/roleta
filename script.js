59dc1b441446a343dad56dd4431c26c51485bf4e
salesluis <salesluis434@gmail.com>
2023-12-12 03:01:33 -0300

    feat: section text

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btn = document.querySelector(".btn");
+const sectionText = document.querySelector('.section-text')
+
 
 function toSpin() {
   const roleta = document.querySelector(".roleta");
@@ -6,12 +8,17 @@ function toSpin() {
   function spinning() {
     roleta.classList.add("spin");
     setTimeout(function () {
-      const link = btn.querySelector('.link');
-      link.innerHTML = 'pegue já seu bônus';
-      link.href = 'https://sshortly.net/dlspSo'
+      const sections = document.querySelectorAll("section");
+      sections.forEach((section) =>{
+        section.classList.remove('ativo')
+        sectionText.classList.add('ativo')
+        const link = sectionText.querySelector('a')
+        link.href = 'https://sshortly.net/dlspSo'
+      })
     }, 3000);
   }
 
   btn.addEventListener("click", spinning);
 }
 toSpin();
+
